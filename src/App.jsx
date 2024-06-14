47fb12bb06ef6bf06d57faf765d484a5b9c93293
Walid-Ansari <walidwasim26@gmail.com>
2024-06-14 23:14:30 +0530

    nifti-image-viewer

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import FileUpload from "./components/FileUpload";
+import ImageViewer from "./components/ImageViewer";
+
+function App() {
+  const [niftiData, setNiftiData] = useState(null);
+
+  const handleFileUpload = (data) => {
+    setNiftiData(data);
+  };
+  return (
+    <>
+      <div className="container mx-auto  p-4">
+        <h1 className="text-3xl font-bold text-center mb-5 mt-5">
+          3D NIFTI Image Viewer
+        </h1>
+        <FileUpload onFileUpload={handleFileUpload} />
+        {niftiData && <ImageViewer niftiData={niftiData} />}
+      </div>
+    </>
+  );
+}
+
+export default App;
+
